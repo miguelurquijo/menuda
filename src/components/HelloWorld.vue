74de5685cc937555e699634b3a45395929aed311
miguelurquijo <miguelurquijo25@gmail.com>
2023-06-28 08:46:22 -0500

    I added the Google Sign in and Sign out

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,60 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1>MENUDA</h1>
+    <p>Is initialized: {{ Vue3GoogleOauth.isInit }}</p>
+    <p>Is authorized in: {{ Vue3GoogleOauth.isAuthorized }}</p>
+    <h2 v-if='user'>User email:  {{ user }}</h2>
+    <button :disabled="!Vue3GoogleOauth.isInit || Vue3GoogleOauth.isAuthorized" @click="handleSignIn">Sign in</button>
+    <button :disabled="!Vue3GoogleOauth.isAuthorized" @click="handleSignOut" >Sign out</button>
   </div>
 </template>
 
+
 <script>
+import { inject } from 'vue';
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+
+data() {
+    return {
+      user: '',
+    }
+},
+  
+  methods: {
+      async handleSignIn() {
+        try {
+          const googleUser = await this.$gAuth.signIn();
+          if(!googleUser){
+            return null;
+        }
+        this.user = googleUser.getBasicProfile().getEmail();
+      } catch (error) {
+          console.log(error);
+          return null;
+        }    
+  },
+
+  async handleSignOut() {
+    try {
+      await this.$gAuth.signOut();
+      this.user = '';
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+},
+
+  setup() {
+    const Vue3GoogleOauth = inject('Vue3GoogleOauth')
+    return { 
+      Vue3GoogleOauth 
+    };
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+import gAuthPlugin from 'vue3-google-oauth2';
+
+
+let gauthClientId = '347237759908-ldk22lfjpe5qre4mv9ej0nls7unvvbro.apps.googleusercontent.com';
+app.use(gAuthPlugin, { 
+    clientId: gauthClientId, 
+    scope: 'email', 
+    prompt: 'consent', 
+    fetch_basic_profile: false, 
+    plugin_name:'Cliente web 2 - menuda'
+});
+app.mount('#app')
