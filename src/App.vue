b01522699dac91b44b06a333ff1295fa8560cfc9
miguelurquijo <miguelurquijo25@gmail.com>
2023-06-28 21:40:21 -0500

    Ajuste readme con las instrucciones para empezar el proyecto

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,6 @@ body {
   color: #2c3e50;
   margin-top: 60px;
   background-color: #fafafa !important;
-
 }
 
 @import url('https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap');
